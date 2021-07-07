f539918dc677b2442d99a932bf8114f25c52fd4c
gominnam <kosok03@naver.com>
2021-07-07 23:59:16 +0900

    Swagger [SwaggerConfig.java] Document, ui

---
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final Contact DEFAULT_CONTACT = new Contact("MINJUN KO",
+            "http://www.cortexKK.co.kr", "kosok03@naver.com");
+
+    @Bean // http://localhost:8088/v2/api-docs
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+
+}
