e268bcf122adef144c4ea0fbed543179d135b718
gominnam <kosok03@naver.com>
2021-07-08 01:17:53 +0900

    Swagger Customizing  [@ApiModel, SwaggerConfig.java]

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -8,19 +8,35 @@ import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDi
 import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     private static final Contact DEFAULT_CONTACT = new Contact("MINJUN KO",
             "http://www.cortexKK.co.kr", "kosok03@naver.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
+            "My User Management TEST API service", "1.0", "urn:tos",
+            DEFAULT_CONTACT, "Apache 2.0",
+            "http://apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml"));
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
 
     @Bean // http://localhost:8088/v2/api-docs
     public LinkDiscoverers discoverers() {
